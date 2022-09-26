57185a6388c91607fd77581232a7658becb823cd
Safa Andac <8544658+SanD94@users.noreply.github.com>
2022-09-26 23:48:09 +0200

    pass enexpr to another function

--- src/advanced-R/playground.R
+++ src/advanced-R/playground.R
@@ -27,3 +27,25 @@ t <- "crysis"
 l <- "crysis"
 ref(t)
 ref(l)
+
+## env and data referencing
+library(tidyverse)
+
+data <- tibble(a = 1:3, b = 6:8, n = c("nope", "hello", "world"))
+
+dummy_func <- function(data, ivs, dv) {
+  print(rlang::enquo(ivs)); print(rlang::enquo(dv))
+  print("hello world!")
+  print(data %>% select({{ ivs }}, {{ dv }}))
+  names <- tidyselect::vars_select(names(data), {{ ivs }})
+  print(names[1])
+  dummy_func_next(data, names[1], {{ dv }})
+}
+
+dummy_func_next <- function(data, iv, dv) {
+  print(rlang::expr(data %>% select({{ iv }}, {{ dv }})))
+  print(data %>% select({{ iv }}, {{ dv }}))
+  print("next hello world")
+}
+
+data %>% dummy_func(c(a, n), b)
